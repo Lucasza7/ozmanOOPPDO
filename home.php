4bcb8525d74d0d2f4c89fffdaa4756de49be3a86
Lucasza7 <133224905+Lucasza7@users.noreply.github.com>
2023-12-11 11:59:32 +0100

    Add files via upload

--- home.php
+++ home.php
@@ -1,11 +1,45 @@
 <?php
 include 'db.php';
-$pdo = new Database();
+$db = new Database();
 
-try {
-    $pdo->insertUser("lucas", "lucas");
-    echo "succes";
-} catch (PDOException $e) {
-    echo "error instert .".$e->getMessage();
-}
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST">
+    <input type="text" name="email" id="">
+    <input type="password" name="password" id="">
+    <input type="submit" value="submit">
+</form>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th colspan="2"> Action</th>
+    </tr>
+
+    <tr>
+        <?php 
+        $users = $db->selectUser();
+        foreach ($users as $user) { ?>
+            <td><?php echo $users['id'];?></td>
+            <td><?php echo $users['email'];?></td>
+            <td><?php echo $users['password'];?></td>
+            <td><a href="edit.php?id=<?php echo $users['id'];?>">Edit</a></td>
+            <td><a href="delete.php?id=<?php echo $users['id'];?>">Delete</a></td>
+    </tr> <?php }?>
+</table>
+
+
+
+</body>
+</html>
